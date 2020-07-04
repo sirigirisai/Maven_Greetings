4ff3e58ee9186558b36b898304366e2aa38e85cf
sai krishna <sirigirisai@gmail.com>
2020-07-04 11:21:36 +0530

    second commit

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -1,11 +1,6 @@
 package hello;
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.junit.Assert.*;
-import org.junit.Test;
-public class GreeterTest {
-private Greeter greeter = new Greeter();
-@Test
-public void greeterSaysHello() {
-assertThat(greeter.sayHello(), containsString("Hello"));
+public class Greeter {
+public String sayHello() {
+return "Hello world!";
 }
 }
